f6ffdd0ccf78e87364c7b714e07f2126606fa22b
quim-romero <quimromero22@gmail.com>
2025-03-13 13:30:00 +0100

    Feat: Add Header to App and add spacing for fixed top nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Container from './components/Container'
 import ThemeToggle from './components/ThemeToggle'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <Container className="py-10 text-center text-2xl font-bold text-primary">
-        Welcome to NebulaOS
-      </Container>
+      <Header />
+      <div className="pt-24">
+        <Container className="py-10 text-center text-2xl font-bold text-primary">
+          Welcome to NebulaOS
+        </Container>
+      </div>
       <ThemeToggle />
     </>
   )
