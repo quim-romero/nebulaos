b383b53a6527ee7e95ec13308b34f29773459541
quim-romero <quimromero22@gmail.com>
2025-03-14 11:30:00 +0100

    Feat: Integrate Hero section into App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Container from './components/Container'
 import ThemeToggle from './components/ThemeToggle'
 import Header from './components/Header'
+import Hero from './sections/Hero'
 
 function App() {
   return (
     <>
       <Header />
-      <div className="pt-24">
+      <main className="pt-24">
+        <Hero />
         <Container className="py-10 text-center text-2xl font-bold text-primary">
           Welcome to NebulaOS
         </Container>
-      </div>
+      </main>
       <ThemeToggle />
     </>
   )
