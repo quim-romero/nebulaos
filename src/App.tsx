78d93bdda985b207a19598c3ca244539b2f41831
quim-romero <quimromero22@gmail.com>
2025-03-15 12:00:00 +0100

    Feat: Add Features section to main App view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Container from './components/Container'
 import ThemeToggle from './components/ThemeToggle'
 import Header from './components/Header'
 import Hero from './sections/Hero'
+import Features from './sections/Features'
 
 function App() {
   return (
@@ -9,9 +9,7 @@ function App() {
       <Header />
       <main className="pt-24">
         <Hero />
-        <Container className="py-10 text-center text-2xl font-bold text-primary">
-          Welcome to NebulaOS
-        </Container>
+        <Features />
       </main>
       <ThemeToggle />
     </>
