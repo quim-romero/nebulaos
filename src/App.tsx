71842c7d261ad93bcabf69c0030ae283f7e6e28b
quim-romero <quimromero22@gmail.com>
2025-03-16 12:00:00 +0100

    Feat: Integrate WhatIs section into App structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ThemeToggle from './components/ThemeToggle'
 import Header from './components/Header'
 import Hero from './sections/Hero'
 import Features from './sections/Features'
+import WhatIs from './sections/WhatIs'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className="pt-24">
         <Hero />
+        <WhatIs />
         <Features />
       </main>
       <ThemeToggle />
