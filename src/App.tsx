15cb595616d787fc4c93dabc27496a76f128b0a4
quim-romero <quimromero22@gmail.com>
2025-03-17 12:00:00 +0100

    Feat: Integrate Testimonials section into App flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import ThemeToggle from './components/ThemeToggle'
 import Header from './components/Header'
+
 import Hero from './sections/Hero'
 import Features from './sections/Features'
 import WhatIs from './sections/WhatIs'
+import Testimonials from './sections/Testimonials'
 
 function App() {
   return (
     <>
       <Header />
-      <main className="pt-24">
-        <Hero />
-        <WhatIs />
-        <Features />
-      </main>
+      <Hero />
+      <WhatIs />
+      <Features />
+      <Testimonials />
       <ThemeToggle />
     </>
   )
