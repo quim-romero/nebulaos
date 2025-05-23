4a7ffd430d6b352eec58b48a233f73a9e6361eab
quim-romero <quimromero22@gmail.com>
2025-05-23 13:20:00 +0200

    chore(ts): add vitest globals to tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    exclude: ['node_modules', 'dist', 'tests'],
+    globals: true,
+    setupFiles: './src/setupTests.ts',
     css: true,
+    include: [
+      'src/**/*.{test,spec}.?(c|m)[jt]s?(x)',
+      'tests/**/*.(spec|test).ts?(x)',
+    ],
   },
 })
