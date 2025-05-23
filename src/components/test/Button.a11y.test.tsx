3aa24cde73494fb72641fe734ecbe013f6682a30
quim-romero <quimromero22@gmail.com>
2025-05-23 17:00:00 +0200

    test(a11y): component a11y test using vitest-axe

--- src/components/test/Button.a11y.test.tsx
+++ src/components/test/Button.a11y.test.tsx
@@ -1,5 +1,6 @@
+import { describe, it, expect } from 'vitest'
 import { render } from '@testing-library/react'
-import { axe } from 'jest-axe'
+import { axe } from 'vitest-axe'
 import Button from '../Button'
 
 describe('Button a11y', () => {
